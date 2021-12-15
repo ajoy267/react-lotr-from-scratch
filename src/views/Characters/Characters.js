ef4a212dc430ea262c6e8d6a02c752d17515d5a5
ajoy267 <andrew.joy12@hotmail.com>
2021-12-14 17:27:12 -0800

    got my characters list to populate

--- src/views/Characters/Characters.js
+++ src/views/Characters/Characters.js
@@ -1,7 +1,23 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { fetchCharacters } from '../../services/characters';
+import CharactersList from '../../components/Characters/CharactersList';
+
 export default function Characters() {
+  const [character, setCharacter] = useState([]);
+
+  useEffect(() => {
+    const getCharacter = async () => {
+      const response = await fetchCharacters('All');
+      setCharacter(response);
+    };
+    getCharacter();
+  }, []);
+
   return (
     <div>
       <h1>Characters</h1>
+      <CharactersList chars={character} />
     </div>
   );
 }
