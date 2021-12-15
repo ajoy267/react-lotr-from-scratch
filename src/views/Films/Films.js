762d2444b1d315d0a8a9001d8b81f1ed49d2d04b
ajoy267 <andrew.joy12@hotmail.com>
2021-12-14 17:48:37 -0800

    adding books info and fetch

--- src/views/Films/Films.js
+++ src/views/Films/Films.js
@@ -1,7 +1,22 @@
+import { useState, useEffect } from 'react';
+import FilmsList from '../../components/Films/FilmsList';
+import { fetchFilms } from '../../services/films';
+
 export default function Films() {
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    const getFilms = async () => {
+      const response = await fetchFilms();
+      setFilms(response);
+    };
+    getFilms();
+  }, []);
+
   return (
     <div>
       <h1>Films</h1>
+      <FilmsList films={films} />
     </div>
   );
 }
